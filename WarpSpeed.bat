626b5df4300f707cd83c4f6ce61820c306ddace5
jdudekay <johnxdudek@gmail.com>
2020-10-07 10:01:29 -0500

    Added 1.0.0 files

---
+++ WarpSpeed.bat
@@ -0,0 +1,116 @@
+@echo off
+rem    WarpSpeed: Night Audit Accelerator - A tool made with the intention of streamlining the renmaming a large amount of randomly-named .pdf files based on their contents
+rem    Copyright (C) 2020 John Dudek
+rem
+rem    This program is free software: you can redistribute it and/or modify
+rem    it under the terms of the GNU General Public License as published by
+rem    the Free Software Foundation, either version 3 of the License, or
+rem    (at your option) any later version.
+rem
+rem    This program is distributed in the hope that it will be useful,
+rem    but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem    GNU General Public License for more details.
+rem
+rem    You should have received a copy of the GNU General Public License
+rem    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+echo +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+echo -+-+-+                                                                                                            +-+-+-
+echo +-+-+-                                                                                                            -+-+-+
+echo -+-+-+                                                                                                            +-+-+-
+echo +-+-+-                                                                                                            -+-+-+
+echo -+-+-+                                                                                                            +-+-+-
+echo +-+-+-                888       888                        .d8888b.                              888              -+-+-+
+echo -+-+-+               888   o   888                       d88P  Y88b                             888               +-+-+-
+echo +-+-+-               888  d8b  888                       Y88b.                                  888               -+-+-+
+echo -+-+-+               888 d888b 888 8888b. 888d88888888b.  "Y888b.  88888b.  .d88b.  .d88b.  .d88888               +-+-+-
+echo +-+-+-               888d88888b888    "88b888P"  888 "88b    "Y88b.888 "88bd8P  Y8bd8P  Y8bd88" 888               -+-+-+
+echo -+-+-+               88888P Y88888.d888888888    888  888      "888888  8888888888888888888888  888               +-+-+-
+echo +-+-+-               8888P   Y8888888  888888    888 d88PY88b  d88P888 d88PY8b.    Y8b.    Y88b 888               -+-+-+
+echo -+-+-+               888P     Y888"Y888888888    88888P"  "Y8888P" 88888P"  "Y8888  "Y8888  "Y88888               +-+-+-
+echo +-+-+-                                           888               888                                            -+-+-+
+echo -+-+-+                                           888               888                                            +-+-+-
+echo +-+-+-                                           888               888                                            -+-+-+
+echo -+-+-+                                                                                                            +-+-+-
+echo +-+-+-                               N i g h t   A u d i t   A c c e l e r a t o r                                -+-+-+
+echo -+-+-+                                                                                                            +-+-+-
+echo +-+-+-                                                   v 1.0.0                                                  -+-+-+
+echo -+-+-+                                                                                                            +-+-+-
+echo +-+-+-                                       Copyright (C) 2020 John Dudek                                        -+-+-+
+echo -+-+-+                                                                                                            +-+-+-
+echo +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+echo +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+pause
+cls
+
+setlocal EnableExtensions DisableDelayedExpansion
+set /a count=1
+
+rem Initialization of OutputFolder and main program prompts
+md "OutputFolder" 2>nul
+echo WarpSpeed: Night Audit Accelerator by John Dudek v 1.0.0
+echo. 
+set /p dat="What is the date you are doing Night Audit for? (MM.DD.YY): "
+
+rem Main program loop
+for /F %%I in ('dir *.pdf /B') do call :RenamePDF "%%~fI"
+
+rem Pause before program exit
+pause
+goto :EOF
+
+:RenamePDF
+set "FilePDF=%~1"
+set "FileTXT=%~dpn1.txt"
+
+rem Create text version of .pdf to be read
+rem Parameters are set to print only the first page and in raw which condenses the data
+pdftotext.exe -l 1 -raw "%FilePDF%"
+
+rem For loop is used to call find which selects the line that contains the report name
+rem Names
+for /F "tokens=1 delims=:" %%A in ('%SystemRoot%\System32\find.exe "Westin Medical Ctr" "%FileTXT%"') do set "report=%%A"
+
+rem The text version of the PDF file is no longer needed.
+del "%FileTXT%"
+
+rem Trim unneccessary words from file name variable
+rem Creates actual fileName variable from user inputted and computer derived variables
+set report=%report:Westin Medical Ctr =%
+set report=%report: Page Number=%
+set "fileName=%report%_%dat%"
+
+rem Move the PDF file to OutputFolder and rename the file while moving it.
+rem Also checks to make sure no files are overwritten
+:while
+if not exist "OutputFolder\%fileName%.pdf" (
+move "%FilePDF%" "OutputFolder\%fileName%.pdf"
+set /a count=1
+) else (
+set "fileName=%report%_%dat% (%count%)"
+set /a count=%count%+1
+goto :while
+)
+
+rem AR reports have the illegal "/" character, this removes it
+rem Also checks to make sure no files are overwritten
+if errorlevel 1 set report=%report:A/R =%
+if errorlevel 1 set "fileName=AR %report%_%dat%"
+if errorlevel 1 (
+:whileAR
+if not exist "OutputFolder\%fileName%.pdf" (
+move "%FilePDF%" "OutputFolder\%fileName%.pdf"
+set /a count=1
+) else (
+set "fileName=%report%_%dat% (%count%)"
+set /a count=%count%+1
+goto :whileAR
+)
+)
+
+rem Exit the subroutine RenamePDF and continue with FOR loop in main code.
+goto :EOF
